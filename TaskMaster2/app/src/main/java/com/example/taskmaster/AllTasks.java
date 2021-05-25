e829a1a8ed757c19aeb3e5848cc8d84065bfc12a
ramayaser66 <ramayaser66@gmail.com>
2021-05-26 01:07:48 +0300

    lab-32

--- TaskMaster2/app/src/main/java/com/example/taskmaster/AllTasks.java
+++ TaskMaster2/app/src/main/java/com/example/taskmaster/AllTasks.java
@@ -5,7 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.Task;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllTasks extends AppCompatActivity {
@@ -14,6 +21,20 @@ public class AllTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
+
+
+        try {
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i("Tutorial", "Initialized Amplify");
+        } catch (AmplifyException e) {
+            Log.e("Tutorial", "Could not initialize Amplify", e);
+        }
+
+
+
+
         RecyclerView taskList = findViewById(R.id.recyclerView);
 
         // the back icon
@@ -21,8 +42,40 @@ public class AllTasks extends AppCompatActivity {
 
 
 
-        List<Tasks> tasks = AppDatabase.getDatabase(getApplicationContext()).tasksDao().getAll();
+        // replace with amplify
+
+        // room
+//        List<Tasks> tasks = AppDatabase.getDatabase(getApplicationContext()).tasksDao().getAll();
+//        taskList.setLayoutManager(new LinearLayoutManager(AllTasks.this));
+//        taskList.setAdapter(new TaskAdapter(AllTasks.this,tasks));
+
+
+
+
         taskList.setLayoutManager(new LinearLayoutManager(AllTasks.this));
-        taskList.setAdapter(new TaskAdapter(AllTasks.this,tasks));
+        Amplify.DataStore.query(Task.class,
+                task -> {
+                    List<Tasks> tasks = new ArrayList<>();
+                    while (task.hasNext()) {
+                        Task todo = task.next();
+                        Tasks newTask = new Tasks();
+
+
+                        newTask.setTitle(todo.getTitle());
+                        newTask.setBody(todo.getDescription());
+                        newTask.setState(todo.getStatus());
+
+                        tasks.add(newTask);
+
+                        taskList.setAdapter(new TaskAdapter(AllTasks.this,tasks));
+
+
+                        Log.i("Tutorial", "==== Todo ====");
+                        Log.i("Tutorial", "Name: " + todo.getTitle());
+                    }
+                },
+                failure -> Log.e("Tutorial", "Could not query DataStore", failure)
+        );
+
     }
 }
